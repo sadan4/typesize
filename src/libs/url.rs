d7abf70e72200fc4d595e231ba5a0d8c55758c53
sadan <117494111+sadan4@users.noreply.github.com>
2026-08-14 23:51:19 -0400

    Update url.rs

--- src/libs/url.rs
+++ src/libs/url.rs
@@ -4,7 +4,6 @@ use crate::TypeSize;
 
 impl TypeSize for url::Url {
     fn extra_size(&self) -> usize {
-        let serialization: String = self.clone().into();
-        serialization.extra_size()
+        self.as_str().len()
     }
 }
