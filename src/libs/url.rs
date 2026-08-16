4e032bc507cc693d3f66aac905ed7df8473f9df7
sadan <117494111+sadan4@users.noreply.github.com>
2026-08-16 15:12:27 -0400

    Remove useless clone in URL implementation (#13)

--- src/libs/url.rs
+++ src/libs/url.rs
@@ -1,10 +1,7 @@
-use alloc::string::String;
-
 use crate::TypeSize;
 
 impl TypeSize for url::Url {
     fn extra_size(&self) -> usize {
-        let serialization: String = self.clone().into();
-        serialization.extra_size()
+        self.as_str().len()
     }
 }
